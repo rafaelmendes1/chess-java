3d4fa289fbf64a661916540c64b8681e371e570e
rafaelmendes1 <rarafafa3245@gmail.com>
2020-09-27 18:05:35 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -12,12 +12,6 @@ public class Rook extends ChessPiece {
     }
 
     @Override
-    public String toString() {
-        return "R";
-    }
-
-
-    @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -64,6 +58,11 @@ public class Rook extends ChessPiece {
         }
 
         return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "R";
     }
 
 }
